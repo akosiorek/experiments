1ea6e7bcc8f7078acf3baeb3b0ef45bb74ec45a3
Adam Kosiorek <a.kosiorek@tum.de>
2016-05-09 18:06:22 +0200

    gauss rank one tests

---
+++ gaus_rank_one_tests.py
@@ -0,0 +1,44 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+theano_flags = os.environ['THEANO_FLAGS'] if 'THEANO_FLAGS' in os.environ else ''
+os.environ['THEANO_FLAGS'] = 'device=cpu,' + theano_flags
+os.environ['GNUMPY_IMPLICIT_CONVERSION'] = 'allow'
+
+
+import theano
+import theano.tensor as T
+from breze.arch.construct.layer.distributions import RankOneGauss
+import numpy as np
+
+
+floatx = theano.config.floatX
+
+dims = 2
+
+mean = np.array([1, 2, 3, 4, 5, 6, 7, 8], dtype=floatx).reshape(dims, dims, dims)
+var = np.array([1, 2, 3, 4, 5, 6, 7, 8], dtype=floatx).reshape(dims, dims, dims)
+uu = np.array([1, 2, 3, 4, 5, 6, 7, 8], dtype=floatx).reshape(dims, dims, dims)
+
+# real_cov = uu[0, ...].T.dot(uu[0, ...]) + np.diagflat(var[0])
+#
+# print real_cov
+
+m, v, u = (T.tensor3(i) for i in ('mean', 'var', 'u'))
+
+
+gaus = RankOneGauss(m, v, u)
+foo = theano.function([m, v, u], gaus.sample(), on_unused_input='warn')
+
+# result = foo(mean, var, uu)
+# print result.shape
+# print result
+
+n = int(1e6)
+X = np.zeros((dims ** 3, n))
+for i in xrange(n):
+    X[:, i] = foo(mean, var, uu).flatten()
+
+print np.cov(X)
+print X.mean(axis=1)
+
+
---
+++ gaussian_transforms.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy import random
+from matplotlib import pyplot as plt
+from scipy.stats import multivariate_normal
+
+
+
+fig = plt.figure()
+ax_gaus1d = fig.add_subplot(2, 2, 1)
+ax_gaus2d = fig.add_subplot(2, 2, 3)
+ax_trans1d = fig.add_subplot(2, 2, 2)
+ax_trans2d = fig.add_subplot(2, 2, 4)
+
+
+x, y = np.mgrid[-1:1:.1, -1:1:.1]
+pos = np.empty(x.shape + (2,))
+pos[:, :, 0] = x
+pos[:, :, 1] = y
+# rv = multivariate_normal([0.5, -0.2], [[2.0, 0.3], [0.3, 0.5]])
+rv = multivariate_normal(mean=None, cov=[[1, 0], [0, 1]])
+pdf = 50 * rv.pdf(pos) - 5
+
+
+cs_gaus = ax_gaus2d.contourf(x, y, pdf)
+cs_trans = ax_trans2d.contourf(x, y, np.sin(pdf) + np.cos(pdf))
+plt.colorbar(cs_gaus, ax=ax_gaus2d)
+plt.colorbar(cs_trans, ax=ax_trans2d)
+
+plt.show()
