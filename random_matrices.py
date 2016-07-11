9c49d0a56f74fbd6157282f5ca426fa649b8c0f4
Adam Kosiorek <a.kosiorek@tum.de>
2016-07-11 18:29:50 +0200

    Work

--- random_matrices.py
+++ random_matrices.py
@@ -10,7 +10,7 @@ def softmax(x):
     return e_x / e_x.sum()
 
 
-N = 100
+N = 10
 n = 49
 m = 16
 # A = randn(N, n, n)
@@ -21,34 +21,45 @@ scale = 0.9 ** (1.0/n)
 for i in xrange(N):
     parts = randn(m, n, n) * 0.1
 
+
     alphas = softmax(randn(m, 1, 1))
     # alphas = np.abs(randn(m, 1, 1))
     # alphas /= alphas.sum()
+    A[i] = (alphas * parts).sum(0)
+    A[i] = np.triu(A[i])
+    print np.diag(A[i])
+    A[i][np.diag_indices(n)] = 1#abs(np.diag(A[i]))
 
-    A[i] = (alphas * parts).sum(0) + np.eye(n) * scale
 
 
-    # precond = np.sqrt((A[i] ** 2).sum(axis=-1, keepdims=True))
-    # print A[i].shape, precond.shape
-    # print np.diag(1/np.diag(A[i])).shape
-    # A[i] = A[i].dot()
+    A[i] = A[i].dot(A[i].T)
+    # # A[i] -= np.eye(n)
 
-    # A[i] = parts.mean(0)
+    # parts = np.zeros(m, n, n)
+    # for j in xrange(m):
+    #     parts = randn(m, n * (n + 1) / 2)
+
+
+
+
 
 print scale
 conds = [cond(a) for a in A]
 dets = np.abs([det(a) for a in A])
 ranks = [matrix_rank(a) for a in A]
-eigs = [gmean(np.abs(eigvals(a))) for a in A]
+eigs = [np.mean(np.abs(eigvals(a))) for a in A]
 
 
 print 'cond:', gmean(conds)
 print 'det:', gmean(dets)
 print 'min rank:', min(ranks)
-print 'avg eig:', gmean(eigs)
+print 'gavg eig:', gmean(eigs)
+print ' avg eig:', np.mean(eigs)
 
 
 # for a, d, c in zip(A, dets, conds):
 #     ord = 1.0 / a.shape[-1]
 #     div = np.abs(d) ** ord
 #     print d, div, det(a / div), c, cond(a / div)
+
+print A[i]
